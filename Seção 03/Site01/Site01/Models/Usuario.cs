de760f95a3e72e45eba4f2c51511f86412158d19
nickcintra <nickcintra@hotmail.com>
2022-03-14 19:13:21 -0300

    user atualização

--- Seção 03/Site01/Site01/Models/Usuario.cs
+++ Seção 03/Site01/Site01/Models/Usuario.cs
@@ -8,8 +8,8 @@ namespace Site01.Models
 {
     public class Usuario
     {
-        [Required(ErrorMessage = "O campo 'Email' é obrigatório!")]
-        [EmailAddress]
+        [Required(ErrorMessage = "O campo 'E-mail' é obrigatório!")]
+        [EmailAddress(ErrorMessage = "O campo 'E-mail' é inválido!")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "O campo 'Senha' é obrigatório!")]
